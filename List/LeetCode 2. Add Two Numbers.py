d405574e79e32d0c3ff5f9c8cef553354c1ebf4f
RijunLiao <30612541+RijunLiao@users.noreply.github.com>
2020-06-03 14:43:26 -0500

    Add files via upload

--- List/LeetCode 2. Add Two Numbers.py
+++ List/LeetCode 2. Add Two Numbers.py
@@ -23,10 +23,20 @@ class Solution:
             tail.next=ListNode(sum_%10)
             tail=tail.next
             carry_bit=sum_//10
-            #if l1.next!=None: l1=l1.next # erro
-            #if l2.next!=None:  l2=l2.next # erro
+            #if l1.next!=None: l1=l1.next # error
+            #if l2.next!=None:  l2=l2.next # error
             if l1!=None: l1=l1.next # right
             if l2!=None:  l2=l2.next # right            
             
         return dummy_head.next
-        
+
+'''
+Time Complexity: O(max(n,m))
+Space Complexity: O(max(n,m))
+
+'''
+
+'''
+Runtime: 72 ms, faster than 70.51% of Python3 online submissions for Add Two Numbers.
+Memory Usage: 13.9 MB, less than 5.67% of Python3 online submissions for Add Two Numbers.
+'''
